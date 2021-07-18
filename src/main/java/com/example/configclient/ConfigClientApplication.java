64d1a9d394565dd14f60779bb718c16b637d0af5
ming.wang4 <ming.wang4@envision-digital.com>
2021-07-18 17:10:20 +0800

    bus enable

--- src/main/java/com/example/configclient/ConfigClientApplication.java
+++ src/main/java/com/example/configclient/ConfigClientApplication.java
@@ -3,6 +3,7 @@ package com.example.configclient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,13 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@RefreshScope
 public class ConfigClientApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ConfigClientApplication.class, args);
     }
 
-    @Value("${user.role}")
+    @Value("${user.role:xx}")
     private String role;
 
 
